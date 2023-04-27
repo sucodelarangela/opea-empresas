54a99d4eefce1bbd6ebf1c21cf0fdb419d59e4bc
Angela Caldas <aes.caldas@gmail.com>
2023-04-27 17:46:30 -0300

    feat: add Modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,67 @@
+import styles from './Modal.module.sass';
+import { IoMdCloseCircle } from 'react-icons/io';
+import { FaTrashAlt } from 'react-icons/fa';
+import { Dispatch, SetStateAction } from 'react';
+
+interface Props {
+  setModalOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const Modal = ({ setModalOpen }: Props) => {
+  return (
+    <>
+      <div className={styles.overlay}></div>
+      <div className={styles.modal}>
+        <div className={styles.modal__header}>
+          <h2>Cadastrar Empresa</h2>
+          <IoMdCloseCircle
+            size={16}
+            onClick={() => setModalOpen(false)}
+          />
+        </div>
+        <form className={styles.modal__form}>
+          <fieldset>
+            <label htmlFor='name'>Nome</label>
+            <input
+              type='text'
+              id='name'
+            />
+          </fieldset>
+          <fieldset>
+            <label htmlFor='cnpj'>CNPJ</label>
+            <input
+              type='text'
+              id='cnpj'
+            />
+          </fieldset>
+          <fieldset>
+            <label htmlFor='email'>E-mail</label>
+            <input
+              type='email'
+              id='email'
+            />
+          </fieldset>
+          <div className={styles.modal__footer}>
+            <button
+              type='button'
+              className={styles.delete}
+            >
+              <FaTrashAlt size={14} />
+            </button>
+            <div>
+              <button
+                type='button'
+                className={styles.cancel}
+                onClick={() => setModalOpen(false)}
+              >Cancelar</button>
+              <button
+                type='submit'
+                className={styles.register}
+              >Cadastrar</button>
+            </div>
+          </div>
+        </form >
+      </div >
+    </>
+  );
+};
--- src/components/Table/Button/index.tsx
+++ src/components/Table/Button/index.tsx
@@ -1,13 +1,25 @@
-import { FaClipboardList } from 'react-icons/fa';
 import styles from './Button.module.sass';
+import { FaClipboardList } from 'react-icons/fa';
+import { Modal } from 'components/Modal';
+import { useState } from 'react';
 
 export const Button = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
   return (
-    <button className={styles.button}>
-      <div className={styles.button__icon}>
-        <FaClipboardList size={20} />
-      </div>
-      <h2 className={styles.button__text}>Adicionar Empresa</h2>
-    </button>
+    <>
+      <button
+        className={styles.button}
+        onClick={() => setModalOpen(!modalOpen)}
+      >
+        <div className={styles.button__icon}>
+          <FaClipboardList size={20} />
+        </div>
+        <h2 className={styles.button__text}>Adicionar Empresa</h2>
+      </button>
+
+      {/* MODAL */}
+      {modalOpen && <Modal setModalOpen={setModalOpen} />}
+    </>
   );
 };
